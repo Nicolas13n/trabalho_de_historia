2870afe6db4bdc1d18a96c149c609d22a36fdc46
Nicolas <nicknagildo1305@gmail.com>
2025-06-26 09:34:50 -0300

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,7 +9,7 @@ export default function Header(){
                 <h1>Tupi-Guarani</h1>
                 <nav>
                     <ul>
-                       <a download="src/components/header/application-52695e6f-b0c3-42f7-8c95-8583ec5ae1e3.apk">App</a>
+                       <a download="application-52695e6f-b0c3-42f7-8c95-8583ec5ae1e3.apk">App</a>
                        <a href="https://youtu.be/tVjQPs1chcs">Video</a>
                     </ul>
                 </nav>
