f5313a1828fcb33eb4146d8dda2fce7a87d003ea
Nicolas <nicknagildo1305@gmail.com>
2025-06-26 09:31:13 -0300

    apk

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,7 +9,7 @@ export default function Header(){
                 <h1>Tupi-Guarani</h1>
                 <nav>
                     <ul>
-                       <a href="quarani 1.apk" download="appHistória.apk">App</a>
+                       <a href="quarani 1" download="src/components/header/application-52695e6f-b0c3-42f7-8c95-8583ec5ae1e3.apk">App</a>
                        <a href="https://youtu.be/tVjQPs1chcs">Video</a>
                     </ul>
                 </nav>
